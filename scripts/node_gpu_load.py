4323f2fd0c5c705ebe0c673bb037122daffe26e4
Zhengheng Li <zhenghenge@gmail.com>
2021-04-21 09:14:38 +0800

    node_gpu_load.py

--- scripts/node_gpu_load.py
+++ scripts/node_gpu_load.py
@@ -4,10 +4,13 @@ import argparse
 import subprocess
 import xml
 import json
+import time
 import re
 import psutil
 
 parser = argparse.ArgumentParser(description='desc')
+parser.add_argument("-i", dest="interval", type=int, default=1)
+parser.add_argument("-r", dest="repeat", type=int, default=1)
 args = parser.parse_args()
 
 ref_gpu_util = re.compile(r"""
@@ -17,14 +20,23 @@ Gpu    \s+ : \s+ (\d+\.?\d*) \s+ % \s+
 Memory \s+ : \s+ (\d+\.?\d*) \s+ %
 """, re.X)
 
-nvidia_smi_output = str(subprocess.check_output(['nvidia-smi', '-q', '-d', 'UTILIZATION']), 'utf-8')
-
-matches = re.findall(ref_gpu_util, nvidia_smi_output)
-
 gpu_load = {}
-for m in matches:
-    gpu_load[m[0]] = {}
-    gpu_load[m[0]]['gpu'] = float(m[1])
-    gpu_load[m[0]]['mem'] = float(m[2])
+
+for x in range(args.repeat):
+    time.sleep(args.interval)
+    nvidia_smi_output = str(subprocess.check_output(['nvidia-smi', '-q', '-d', 'UTILIZATION']), 'utf-8')
+    matches = re.findall(ref_gpu_util, nvidia_smi_output)
+    for m in matches:
+        if (m[0] in gpu_load):
+            gpu_load[m[0]]['gpu'] += float(m[1])
+            gpu_load[m[0]]['mem'] += float(m[2])
+        else:
+            gpu_load[m[0]] = {}
+            gpu_load[m[0]]['gpu'] = float(m[1])
+            gpu_load[m[0]]['mem'] = float(m[2])
+
+for x in gpu_load:
+    gpu_load[x]['gpu'] /= args.repeat
+    gpu_load[x]['mem'] /= args.repeat
 
 print(json.dumps(gpu_load))
