ad12ea09c743246a2f4bce0f3ce20c3fdd75aa67
Zhengheng Li <zhenghenge@gmail.com>
2021-04-23 17:07:41 +0800

    report_user_cpu_mem.py

--- scripts/report_user_cpu_mem.py
+++ scripts/report_user_cpu_mem.py
@@ -4,16 +4,20 @@ import argparse
 import time
 import json
 import subprocess
+import requests
 from multiprocessing import Pool
 from datetime import datetime
 
 parser = argparse.ArgumentParser(description='desc')
 parser.add_argument("-i", dest="interval", type=int, default=1)
 parser.add_argument("-r", dest="repeat", type=int, default=1)
-parser.add_argument("--url", dest="url", type=str, default=1)
+parser.add_argument("--url", dest="url", type=str, default='http://localhost:3000/user-cpu-mem')
 args = parser.parse_args()
 
 script_path = "/home/mark/GitHub/cluster-monitoring/scripts/collect_user_cpu_mem.py"
 output = json.loads(subprocess.check_output([script_path, '-i', str(args.interval), '-r', str(args.repeat)]))
 
-print(json.dumps(output))
+res = requests.post(args.url, json=output)
+
+print(res.status_code)
+print(res.text)
