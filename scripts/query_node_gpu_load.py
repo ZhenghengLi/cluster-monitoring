ab617dcb7ddb521f8c4e5536212aca24326694f3
Zhengheng Li <zhenghenge@gmail.com>
2021-04-28 14:13:02 +0800

    set password by command line option

--- scripts/query_node_gpu_load.py
+++ scripts/query_node_gpu_load.py
@@ -5,8 +5,6 @@ import time
 import json
 import requests
 
-password = 'xxxx'
-
 default_max_time = round(time.time() * 1000)
 default_min_time = default_max_time - 3 * 60 * 1000
 
@@ -14,6 +12,7 @@ parser = argparse.ArgumentParser(description='desc')
 parser.add_argument("--url", dest="url", type=str, default='http://localhost:3000/node-gpu-load')
 parser.add_argument("--minTime", dest="minTime", type=int, default=default_min_time)
 parser.add_argument("--maxTime", dest="maxTime", type=int, default=default_max_time)
+parser.add_argument("--password", dest="password", type=str, default='xxxx')
 args = parser.parse_args()
 
 query_obj = {
@@ -25,7 +24,7 @@ query_obj = {
 }
 
 url = args.url + '?filter=' + requests.utils.quote(json.dumps(query_obj))
-res = requests.get(url, headers={'authorization': password})
+res = requests.get(url, headers={'authorization': args.password})
 
 print(res.status_code)
 print(res.text)
--- scripts/report_user_cpu_mem.py
+++ scripts/report_user_cpu_mem.py
@@ -8,18 +8,17 @@ import requests
 from multiprocessing import Pool
 from datetime import datetime
 
-password = 'xxxx'
-
 parser = argparse.ArgumentParser(description='desc')
 parser.add_argument("-i", dest="interval", type=int, default=1)
 parser.add_argument("-r", dest="repeat", type=int, default=1)
 parser.add_argument("--url", dest="url", type=str, default='http://localhost:3000/user-cpu-mem')
+parser.add_argument("--password", dest="password", type=str, default='xxxx')
 args = parser.parse_args()
 
 script_path = "/home/mark/GitHub/cluster-monitoring/scripts/collect_user_cpu_mem.py"
 output = json.loads(subprocess.check_output([script_path, '-i', str(args.interval), '-r', str(args.repeat)]))
 
-res = requests.post(args.url, json=output, headers={'authorization': password})
+res = requests.post(args.url, json=output, headers={'authorization': args.password})
 
 print(res.status_code)
 print(res.text)
