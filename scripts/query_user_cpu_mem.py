d8abc22174299d78278636fdb003c5e6afd08e1a
Zhengheng Li <zhenghenge@gmail.com>
2021-04-28 10:11:08 +0800

    query_user_cpu_mem.py

--- scripts/query_user_cpu_mem.py
+++ scripts/query_user_cpu_mem.py
@@ -7,13 +7,20 @@ import requests
 
 password = 'xxxx'
 
+default_max_time = round(time.time() * 1000)
+default_min_time = default_max_time - 3 * 60 * 1000
+
 parser = argparse.ArgumentParser(description='desc')
 parser.add_argument("--url", dest="url", type=str, default='http://localhost:3000/user-cpu-mem')
+parser.add_argument("--minTime", dest="minTime", type=int, default=default_min_time)
+parser.add_argument("--maxTime", dest="maxTime", type=int, default=default_max_time)
 args = parser.parse_args()
 
 query_obj = {
     'where': {
-        'completed': False
+        'time': {
+            'between': [args.minTime, args.maxTime]
+        }
     }
 }
 
