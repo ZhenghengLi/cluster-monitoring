ad74a6ec6e50fc32aabf009df6d4f2e151df94ae
Zhengheng Li <zhenghenge@gmail.com>
2021-05-07 14:32:30 +0800

    node_cpu_load.py: add memory usage

--- scripts/node_cpu_load.py
+++ scripts/node_cpu_load.py
@@ -14,17 +14,21 @@ psutil.cpu_times_percent()
 
 attr_list = ['user', 'system', 'idle', 'iowait', 'irq', 'softirq', 'steal', 'guest']
 
-cpu_times = {}
+cpu_load = {}
 for attr in attr_list:
-    cpu_times[attr] = 0.0
+    cpu_load[attr] = 0.0
+
+cpu_load['mem'] = 0.0
 
 for x in range(args.repeat):
     time.sleep(args.interval)
     ctp = psutil.cpu_times_percent()
     for attr in attr_list:
-        cpu_times[attr] += getattr(ctp, attr)
+        cpu_load[attr] += getattr(ctp, attr)
+    svm = psutil.virtual_memory()
+    cpu_load['mem'] += svm.percent
 
-for attr in attr_list:
-    cpu_times[attr] /= args.repeat
+for attr in cpu_load:
+    cpu_load[attr] /= args.repeat
 
-print(json.dumps(cpu_times))
+print(json.dumps(cpu_load))
